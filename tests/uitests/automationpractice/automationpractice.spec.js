7294336b7ac057e1a87fa86dbcad689d6df376fd
aravindms <aravind.ms@modivcare.com>
2023-11-26 23:11:48 +0530

    Tenth Commit

--- tests/uitests/automationpractice/automationpractice.spec.js
+++ tests/uitests/automationpractice/automationpractice.spec.js
@@ -8,7 +8,7 @@ test.afterEach(async ({ page }) => {
     await page.close()
 });
 
-test('Test Visibility', async ({ page }) => {
+test('@UI Test Visibility', async ({ page }) => {
     await page.locator('#displayed-text').fill('Hello')
     await expect(page.getByPlaceholder('Hide/Show Example')).toBeVisible()
     await page.getByRole('button', { name: 'Hide' }).click();
@@ -18,13 +18,13 @@ test('Test Visibility', async ({ page }) => {
     console.log('Test Visibility Passed')
 });
 
-test('Test Mousehover', async ({ page }) => {
+test('@UI Test Mousehover', async ({ page }) => {
     await page.locator('#mousehover').hover()
     await expect(page.locator("a[href='#top']")).toBeVisible()
     console.log('Test Mousehover Passed')
 });
 
-test('Test Autosuggestions', async ({ page }) => {
+test('@UI Test Autosuggestions', async ({ page }) => {
     await page.locator('#autocomplete').fill('Ma')
     await page.waitForSelector('.ui-menu-item-wrapper', { visible: true })
     const suggestions1 = await page.$$('.ui-menu-item-wrapper');
@@ -37,7 +37,7 @@ test('Test Autosuggestions', async ({ page }) => {
     console.log('Test Autosuggestions Passed')
 });
 
-test('Test Dialogs', async ({ page }) => {
+test('@UI Test Dialogs', async ({ page }) => {
     await page.getByPlaceholder('Enter Your Name').fill('Aravind');
     page.once('dialog', dialog => {
       expect(dialog.message()).toBe('Hello Aravind, Are you sure you want to confirm?')
@@ -53,7 +53,7 @@ test('Test Dialogs', async ({ page }) => {
     console.log('Test Dialogs Passed')
 });
 
-test('Test Frames', async ({ page }) => {
+test('@UI Test Frames', async ({ page }) => {
     const frame = page.frameLocator('#courses-iframe')
     await frame.getByRole('link', { name: ' More ' }).click();
     await frame.getByRole('link', { name: 'About us', exact: true }).click();
@@ -61,7 +61,7 @@ test('Test Frames', async ({ page }) => {
     console.log('Test Frames Passed')
 });
 
-test('Test WebTables', async ({ page }) => {
+test('@UI Test WebTables', async ({ page }) => {
     let amount = 0
     for(let i=1;i<10;i++)
     {
@@ -71,12 +71,12 @@ test('Test WebTables', async ({ page }) => {
     console.log('Test WebTables Passed')
 });
 
-test('Test Screenshot', async ({ page }) => {
+test('@UI Test Screenshot', async ({ page }) => {
     await page.screenshot({ path: 'tests/screenshots/screenshot.png', fullPage: true })
     console.log('Test Screenshot Passed')
 });
 
-test('Test Element Screenshot', async ({ page }) => {
+test('@UI Test Element Screenshot', async ({ page }) => {
     await page.locator('#mousehover').screenshot({ path: 'tests/screenshots/elementscreenshot.png' });
     console.log('Test Element Screenshot Passed')
 });
